0befdfa90c5f8abc6f258004b2d39879be466853
Jose Patino <josepatino4576@gmail.com>
2025-02-13 15:41:12 -0800

    Inital Commit

---
+++ divi-child/doula_cards.php
@@ -0,0 +1,137 @@
+<?php
+if ( ! function_exists( 'staff_cards' ) ) {	
+    function staff_cards() {
+		
+		if ($_GET['search'] && !empty($_GET['search'])){
+		$search = $_GET['search'];
+	}
+	
+		if ($_GET['language'] && !empty($_GET['language'])){
+			$language = $_GET['language'];
+		}
+
+		if ($_GET['location'] && !empty($_GET['location'])){
+			$location = $_GET['location'];
+		}
+		
+		if ($_GET['due_date'] && !empty($_GET['due_date'])){
+			$due_date = $_GET['due_date'];
+		}
+
+		if ($_GET['insurance'] && !empty($_GET['insurance'])){
+			$insurance = $_GET['$insurance'];
+		}
+        $args = array(
+            'posts_per_page' => -1,
+            'post_type'     	=> 'staff',
+			
+		);
+		if (!empty($search)) {
+			$args['s'] = $search;
+		}
+		
+		// Only add meta query if language is selected
+		else if (!empty($language) && !empty($location) && !empty($YOE)) {
+			$args['meta_query'] = array(	
+				'relation' => 'AND',
+				array(
+					'key'     => 'language',
+					'value'   => '"' . $language . '"',
+					'compare' => 'LIKE'
+				),
+				array(
+					'key'     => 'location',
+					'value'   => $location,
+					'compare' => 'LIKE'
+				),
+				array(
+					'key'     => 'years_of_experience',
+					'value'   => $YOE,
+					'compare' => 'LIKE'
+				),
+			);
+		} else if (!empty($language)) {
+			$args['meta_query'] = array(			
+				array(
+					'key'     => 'language',
+					'value'   => '"' . $language . '"',
+					'compare' => 'LIKE'
+				),
+			);
+		} else if (!empty($location)) {
+			$args['meta_query'] = array(			
+				array(
+					'key'     => 'location',
+					'value'   => $location,
+					'compare' => 'LIKE'
+				),
+			);
+		} else if (!empty($YOE)) {
+			$args['meta_query'] = array(			
+				array(
+					'key'     => 'years_of_experience',
+					'value'   => $YOE,
+					'compare' => '='
+				),
+			);
+		}
+		
+        $recent_posts = new WP_Query($args);
+
+        if ($recent_posts->have_posts()) {
+            echo "<div class='staff-container'>";
+            while ($recent_posts->have_posts()) {
+                $recent_posts->the_post();
+                $post_id = get_the_ID();
+				
+				//Get Default Image from Media Library
+				$upload_dir = wp_upload_dir();
+				$default_image = $upload_dir['baseurl'] . '/2024/07/doula.webp';
+				$image_url = get_the_post_thumbnail_url($post_id, 'full') ?: $default_image;
+				
+                $name = get_field('name', $post_id);
+                $bio = get_field('bio', $post_id);
+                $position = get_field('position', $post_id);
+				$more_details = get_field('additional_info', $post_id);
+				$is_staff_unavailable = get_field('not_available', $post_id);
+
+				if($is_staff_unavailable == 1) {
+					continue;
+				}
+?>
+
+                    <div class="staff-item">
+                        <div class="staff-image-container">
+                            <img src="<?php echo $image_url; ?>" alt="<?php echo $name; ?>" class="staff-image" />
+                        </div>
+
+                        <h2 class="staff-header">
+                            <?php echo $name; ?>
+                        </h2>
+
+                        <p class="staff-position">
+                            <?php echo $position; ?>
+                        </p>
+
+                        <details name="staff-<?php echo $post_id ?>" class="accordion-trigger">
+                            <summary class="accordion-summary">Biography</summary>
+
+                            <p class="accordion-content"><?php echo $bio ?></p>
+                        </details>
+                        <details name="staff-<?php echo $post_id ?>" class="accordion-trigger">
+                            <summary class="accordion-summary">More Info</summary>
+                            <p class="accordion-content"><?php echo $more_details ?></p>
+                        </details>
+                    </div>
+
+                <?php
+            }
+            echo "</div>";
+        } else {
+            echo '<p>No staff found.</p>';
+        }
+        wp_reset_postdata();
+    }
+    staff_cards();
+}
+?>
---
+++ divi-child/functions.php
@@ -0,0 +1,26 @@
+<?php
+add_action('wp_enqueue_scripts', 'divi_child_enqueue_scripts');
+function divi_child_enqueue_scripts(){
+    wp_enqueue_style('divi-child-parent-css', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('divi-child-child-css', get_stylesheet_uri());
+
+}
+
+function doula_cards_shortcode() {
+    // Include your PHP file from the child theme directory
+    ob_start();
+    include(get_stylesheet_directory() . '/doula_cards.php');
+    return ob_get_clean();
+}
+add_shortcode('doula_cards', 'doula_cards_shortcode');
+
+function custom_title_placeholder($title) {
+    $screen = get_current_screen();
+    
+    if ('staff' === $screen->post_type) {
+        $title = 'Enter Staff Name...'; // Replace with your desired placeholder text
+    }
+
+    return $title;
+}
+add_filter('enter_title_here', 'custom_title_placeholder');
