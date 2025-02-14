f2eaa39d4a9a1258a64686273c04d1e4d9ae3cff
Jose Patino <josepatino4576@gmail.com>
2025-02-14 11:04:42 -0800

    Moved Doula Card stylings to Local

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -1,10 +1,11 @@
 <?php
-add_action('wp_enqueue_scripts', 'divi_child_enqueue_scripts');
 function divi_child_enqueue_scripts(){
     wp_enqueue_style('divi-child-parent-css', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('divi-child-child-css', get_stylesheet_uri());
-
+    wp_enqueue_style('doula-cards-css', get_stylesheet_directory_uri() . '/doula_cards.css');
 }
+
+add_action('wp_enqueue_scripts', 'divi_child_enqueue_scripts');
 
 function doula_cards_shortcode() {
     // Include your PHP file from the child theme directory
