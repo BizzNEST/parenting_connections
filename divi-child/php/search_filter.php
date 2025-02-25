d74806ea4f6450acaa4c4d6cd8314e845aef1b20
Jacky Perez <jackyyp2402@gmail.com>
2025-02-25 09:42:33 -0800

    feat(toggle): modal can be toggled

--- divi-child/php/search_filter.php
+++ divi-child/php/search_filter.php
@@ -27,93 +27,98 @@ $insurance = isset($_GET['insurance']) ? $_GET['insurance'] : '';
   </div>
 
 
-
+ 
   <div class="filter-btns-container">
-    <button class="search-filter-btn">
+ 
+    <button class="search-filter-btn" id="filter-btn">
       Filter
     </button>
-    <form action="doula-hub" class="filter-container">
-      <div class="edit-modal-content" id="edit-modal-content">
+   
+      <form action="doula-hub" class="filter-container">
+        <div class="edit-modal-content" id="edit-modal-content">
 
-        <div class="modal-header">
-          <div>
-            <span class="edit-close">&times;</span>
+          <div class="modal-header">
+            <div>
+              <span class="edit-close">&times;</span>
+            </div>
+            <div class="title-clear-button">
+              <p id="edit-modal-header">Filter By</p>
+              <button class="clear-button">
+                Clear All
+              </button>
+            </div>
           </div>
-          <div class="title-clear-button">
-            <p id="edit-modal-header">Filter By</p>
-            <button class="clear-button">
-              Clear All
-            </button>
-          </div>
+
+          <fieldset>
+            <legend class="label">Langauage:</legend>
+            <div>
+              <input type="checkbox" id="English" name="language" value="English" />
+              <label for="English">English</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Spanish" name="language" value="Spanish" />
+              <label for="Spanish">Spanish</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Mixteco" name="language" value="Mixteco" />
+              <label for="Mixteco">Mixteco</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Triqui" name="language" value="Triqui" />
+              <label for="Triqui">Triqui</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Zapotec" name="language" value="Zapotec" />
+              <label for="Zapotec">Zapotec</label>
+            </div>
+
+            <legend class="label">Location:</legend>
+            <div>
+              <input type="checkbox" id="Salinas" name="location" value="Salinas" />
+              <label for="Salinas">Salinas</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Marina" name="location" value="Marina" />
+              <label for="Marina">Marina</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Monterey" name="location" value="Monterey" />
+              <label for="Monterey">Monterey</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Other-loc" name="location" value="Other-loc" />
+              <label for="Other-loc">Other</label>
+            </div>
+
+            <legend class="label">Insurance:</legend>
+            <div>
+              <input type="checkbox" id="MediCal" name="insurance" value="MediCal" />
+              <label for="MediCal">MediCal</label>
+            </div>
+            <div>
+              <input type="checkbox" id="Other" name="insurance" value="Other" />
+              <label for="Other">Other</label>
+            </div>
+
+            <legend class="label">Due Date in:</legend>
+            <div>
+              <input type="checkbox" id="one2two" name="due-date" value="one2two" />
+              <label for="one2two">1-2 months</label>
+            </div>
+            <div>
+              <input type="checkbox" id="three2six" name="due-date" value="three2six" />
+              <label for="three2six">3-6 months</label>
+            </div>
+            <div>
+              <input type="checkbox" id="seven2nine" name="due-date" value="seven2nine" />
+              <label for="seven2nine">6-9 months</label>
+            </div>
+          </fieldset>
+          <button type="submit" id="submit-modal">Submit</button>
         </div>
 
-        <fieldset>
-          <legend class="label">Langauage:</legend>
-          <div>
-            <input type="checkbox" id="English" name="language" value="English" />
-            <label for="English">English</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Spanish" name="language" value="Spanish" />
-            <label for="Spanish">Spanish</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Mixteco" name="language" value="Mixteco" />
-            <label for="Mixteco">Mixteco</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Triqui" name="language" value="Triqui" />
-            <label for="Triqui">Triqui</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Zapotec" name="language" value="Zapotec" />
-            <label for="Zapotec">Zapotec</label>
-          </div>
-
-          <legend class="label">Location:</legend>
-          <div>
-            <input type="checkbox" id="Salinas" name="location" value="Salinas" />
-            <label for="Salinas">Salinas</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Marina" name="location" value="Marina" />
-            <label for="Marina">Marina</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Monterey" name="location" value="Monterey" />
-            <label for="Monterey">Monterey</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Other-loc" name="location" value="Other-loc" />
-            <label for="Other-loc">Other</label>
-          </div>
-
-          <legend class="label">Insurance:</legend>
-          <div>
-            <input type="checkbox" id="MediCal" name="insurance" value="MediCal" />
-            <label for="MediCal">MediCal</label>
-          </div>
-          <div>
-            <input type="checkbox" id="Other" name="insurance" value="Other" />
-            <label for="Other">Other</label>
-          </div>
-
-          <legend class="label">Due Date in:</legend>
-          <div>
-            <input type="checkbox" id="one2two" name="due-date" value="one2two" />
-            <label for="one2two">1-2 months</label>
-          </div>
-          <div>
-            <input type="checkbox" id="three2six" name="due-date" value="three2six" />
-            <label for="three2six">3-6 months</label>
-          </div>
-          <div>
-            <input type="checkbox" id="seven2nine" name="due-date" value="seven2nine" />
-            <label for="seven2nine">6-9 months</label>
-          </div>
-        </fieldset>
-        <button type="submit" id="submit-modal">Submit</button>
-      </div>
-
-    </form>
+      </form>
+      
+   
+    
   </div>
--- divi-child/scripts.js
+++ divi-child/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function observeGTranslate() {
     // Find all language selector links excluding the main toggle
     const languageLinks = document.querySelectorAll(
-      "a.glink.nturl:not(.notranslate)",
+      "a.glink.nturl:not(.notranslate)"
     );
 
     if (languageLinks.length > 0) {
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         link.addEventListener("click", function () {
           console.log(
             "Language link clicked:",
-            this.getAttribute("data-gt-lang"),
+            this.getAttribute("data-gt-lang")
           );
 
           // Make sure we give GTranslate time to translate
@@ -43,7 +43,9 @@ document.querySelector("form").addEventListener("submit", function (event) {
 
   // Redirect with clean parameters
   const queryString = params.toString();
-  const url = `${window.location.pathname}${queryString ? "?" + queryString : ""}`;
+  const url = `${window.location.pathname}${
+    queryString ? "?" + queryString : ""
+  }`;
   window.location.href = url;
 });
 
@@ -52,3 +54,39 @@ function clearFilters() {
   event.preventDefault();
   window.location.href = window.location.pathname;
 }
+
+document
+  .getElementById("filter-btn")
+  .addEventListener("click", function (event) {
+    //alert("button clicked");
+    const modal = document.getElementById("edit-modal-content");
+    //const overlay = document.getElementById("modal-overlay");
+
+    //display the modal
+    //overlay.style.display = "block";
+    //overlay.style.backgroundColor = "pink";
+    modal.style.display = "block";
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("edit-close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+      //overlay.style.backgroundColor = "transparent";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+
+    /*window.addEventListener("click", function (event) {
+      if (modal.style.display === "block" && event.target !== modal) {
+        modal.style.display = "none";
+        //overlay.style.backgroundColor = "transparent";
+      }
+    });*/
+
+    document.getElementById("submit-modal").addEventListener("click", () => {
+      modal.style.display = "none";
+      //overlay.style.backgroundColor = "transparent";
+    });
+  });
