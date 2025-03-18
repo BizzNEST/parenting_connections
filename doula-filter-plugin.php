61095f5a4ac3dcd432bab1810f225c75a1a0c6d7
Jose Patino <josepatino4576@gmail.com>
2025-03-18 09:44:54 -0700

    Removing unnecessary file path inits

--- doula-filter-plugin.php
+++ doula-filter-plugin.php
@@ -18,13 +18,11 @@ if (!class_exists("DoulaFilterPlugin")) {
         {
             define("MY_PLUGIN_PATH", plugin_dir_path(__FILE__));
             define("MY_PLUGIN_URL", plugin_dir_url(__FILE__));
-            require_once(MY_PLUGIN_PATH . '/vendor/autoload.php');
         }
 
         public function initialize()
         {
             include_once MY_PLUGIN_PATH . 'includes/utils.php';
-            include_once MY_PLUGIN_PATH . 'includes/options-page.php';
             include_once MY_PLUGIN_PATH . 'includes/doula-filter.php';
         }
     }
